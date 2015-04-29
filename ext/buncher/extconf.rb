5d249201b6416d9568d0ac9f3bb22a180b9c9311
Robert Mathews <rob@justsoftwareconsulting.com>
2015-04-29 13:30:24 -0400

    documention reason for compiler switch

--- ext/buncher/extconf.rb
+++ ext/buncher/extconf.rb
@@ -4,6 +4,11 @@ require 'mkmf'
 # Give it a name
 extension_name = 'buncher'
 
+# still necessary in order to avoid compile errors like this:
+# https://gist.github.com/48a9a212fd8f9011009f
+# isnan - not a member of std:
+# random library - doesn't exist
+# and other incompatibilities
 self.class::CONFIG['CXX'] = "#{self.class::CONFIG['CXX']} -std=c++11"
 # The destination
 dir_config(extension_name)
